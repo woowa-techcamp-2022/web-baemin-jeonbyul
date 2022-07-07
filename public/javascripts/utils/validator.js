62daf3395ddad72acfbda09c7cc0f0224d363796
jstella96 <jstella0530@naver.com>
2022-07-08 02:46:30 +0900

    [#4] feat: 유효성 검증 util userId 검증 구현

--- public/javascripts/utils/validator.js
+++ public/javascripts/utils/validator.js
@@ -2,25 +2,21 @@ export const isEmaill = (email) => {
 
     const reg_email = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
 
-    if(!reg_email.test(email)){
-        return false;
-    }
+    return reg_email.test(email)
 
-    return true ;
-    
 }
 
-export const isUserId = (str) => {
-
+export const isUserId = (userId) => {
+    
+    const reg_userId = /^[a-z]+[a-z0-9]{5,19}$/g;
+    
+    return reg_userId.test(userId)
+    
 }
 
 export const isPassword = (password) => {
     
-    const req_password = /^[a-zA-Z0-9]{8,16}$/;
+    const reg_password = /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+]).{8,16}$/;
     
-    if(!req_password.test(password)){
-        return false 
-    }
-
-    return true
+    return reg_password.test(password)
 }
